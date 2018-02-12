9b212b2b073b93022893ec2c52cbba511c765308
Fabiano Gomes <fabianojgaraujo@gmail.com>
2018-02-12 19:29:08 -0200

    'ajustes'

--- www/js/listaTemas.js
+++ www/js/listaTemas.js
@@ -5,16 +5,18 @@ function listaTemas() {
 
     $("#temas").append("Carregando...");
 
-alert("0")
+    alert("0") ;
+
     $.getJSON(url,function(result){
 
       // console.log(result);
-      alert("1")
+      alert("1") ;
 
+      /*
       $("#temas").html("");      
       $.each(result, function(i, field){
 
-        alert("2")
+        alert("2") ;
 
         var titulo = field.titulo;
         var descricao = field.descricao;
@@ -22,12 +24,15 @@ alert("0")
         var id_tema_redacao = field.id_tema_redacao;
         
         $("#temas").append("<li class='swipeout'> <div class='swipeout-content item-content'>   <div class='post_entry'>       <div class='post_thumb'><img src='"+imagem+"' alt=' title=' /></div>       <div class='post_details'>            <div class='post_category'><a href='detalhesTema.html' onClick='exibeTema("+id_tema_redacao+")'>"+titulo+"</a></div>                   <h2><a href='detalhesTema.html' onClick='exibeTema("+id_tema_redacao+")'>"+descricao+"</a></h2>          </div>       <div class='post_swipe'><img src='images/swipe_more.png' alt=' title=' /></div>   </div> </div> <div class='swipeout-actions-right'>   <a href='detalhesTema.html' onClick='exibeTema("+id_tema_redacao+")' class='action1'><img src='images/icons/black/message.png' alt=' title=' /></a>      <a href='detalhesTema.html' onClick='exibeTema("+id_tema_redacao+")' class='action1 open-popup' data-popup='.popup-social'><img src='images/icons/black/like.png' alt=' title=' /></a>      <a href='detalhesTema.html' onClick='exibeTema("+id_tema_redacao+")' class='action1 open-popup' data-popup='.popup-social'><img src='images/icons/white/contact.png' alt=' title=' /></a> </div></li>");
-      
       });
+
+      
+        */
+
 
     });
 
-    alert("fim")
+    alert("fim") ;
 
 }
 
